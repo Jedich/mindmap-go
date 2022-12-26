40e13f88e5a6b8811ff1ecb93c18cc68936151df
Jedich <alexyudakov483@gmail.com>
2022-12-26 21:28:56 +0200

    text wrapping

--- frontend/src/components/Canvas.vue
+++ frontend/src/components/Canvas.vue
@@ -7,15 +7,19 @@ import * as d3 from "d3";
 
 export default {
 	mounted() {
-		const width = 500;
+		const width = 800;
 		const data = {
-			name: "a",
+			name: "text text text text text text text text text",
 			children: [
 				{
-					name: "b",
+					name: "123490\n1123456711234 567890112345678901",
 					children: [{
 						name: "b",
 						color: "green",
+						children: []
+					},
+					{
+						name: "c",
 						children: []
 					},
 					{
@@ -112,7 +116,7 @@ export default {
 				});
 
 			const nodeWidth = 150
-			const nodeHeight = 80
+			const nodeHeight = 50
 			const nodeShape = nodeEnter.append('rect')
 				.attr('x', -nodeWidth / 2)
 				.attr('y', -nodeHeight / 2)
@@ -123,13 +127,7 @@ export default {
 				.attr("fill", d => d._children ? "#fff" : "#eee")
 				.attr("opacity", 1)
 				.attr("stroke-width", 5);
-				nodeEnter.append('foreignObject')
-				.attr('x', -nodeWidth / 2 + 10)
-				.attr('y', -nodeHeight / 2 + 3)
-				.attr('width', nodeWidth - 20)
-				.attr('height', nodeHeight -6)
-				.append("xhtml:body")
-				.html('<div style="text-align:center;background-color: transparent;">This is ddd ddd dd ddddd </div>')
+
 			nodeEnter.append("circle")
 				.attr("cx", nodeWidth / 2)
 				.attr("r", 10)
@@ -137,21 +135,62 @@ export default {
 				.attr("stroke", "#ddd")
 				.attr("stroke-width", 3);
 
-
-
 			nodeEnter.append("text")
-				// .attr("dy", "0.31em")
-				.attr("x", -50)
-				.attr("y", -50)
+				.attr("x", 0)
+				.attr("y", d => { d.data.wrappedText = wrapRecu(d.data.name, 13); return -15 - (d.data.wrappedText.length - 1) * 10 })
+				.attr("dy", "0em")
+				// .clone(true)
+				// .lower()
 				.attr("font-size", 20)
-				.attr("text-align", "center")
-				// .attr("text-anchor", d => d._children ? "end" : "start")
-				.text(d => d.data.name)
-				.clone(true)
-				.lower()
-				.attr("stroke-linejoin", "round")
-				.attr("stroke-width", 3)
-				.attr("stroke", "white");
+				.text(d => d.data.wrappedText.join("/"));
+
+			wrap(nodeEnter.selectAll('text'), 5);
+
+			function wrapRecu(text, limit, resultArr = []) {
+				if (text.length > limit) {
+					// find the last space within limit
+					var line = text.slice(0, limit)
+					var len = line.length
+					var arr = [' ', '\n']
+					arr.forEach(element => {
+						var edge = line.lastIndexOf(element);
+						if (edge > 0) {
+							line = text.slice(0, edge);
+							len = edge + 1
+						}
+					});
+					var remainder = text.slice(len);
+					resultArr.push(line)
+					return wrapRecu(remainder, limit, resultArr);
+				} else {
+					resultArr.push(text)
+				}
+				if (resultArr.length === 0) {
+					return [text]
+				}
+				return resultArr;
+			}
+
+			function wrap(text, width) {
+				text.each(function () {
+					var text = d3.select(this),
+						words = text.text().split("/"),
+						lineNumber = 0,
+						lineHeight = 1, // ems
+						x = text.attr("x"),
+						y = text.attr("y"),
+						dy = parseFloat(text.attr("dy")),
+						t = text.attr("y", 500),
+						tspan = text.text(null).append("tspan").attr("text-anchor", "middle").attr("x", x).attr("y", y).attr("dy", dy + "em");
+					words.forEach(word => {
+						tspan = text.append("tspan").attr("x", x).attr("text-anchor", "middle").attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+					});
+					// find corresponding rect and reszie
+					var h = 50 + ((lineNumber - 1) * 17)
+					d3.select(this.parentNode.children[0]).attr('height', h).attr('y', -h / 2);
+
+				});
+			}
 
 			// Transition nodes to their new position.
 			const nodeUpdate = node.merge(nodeEnter).transition(transition)
@@ -178,16 +217,21 @@ export default {
 					// 	"C" + (d.source.y + 200) + "," + d.target.x +
 					// 	" " + (d.source.y + 100) + "," + d.source.x +
 					// 	" " + d.source.y + "," + d.source.x;
-				});
+				})
 
 			// Transition links to their new position.
 			link.merge(linkEnter).transition(transition)
-				.attr("d", diagonal);
+				.attr("d", d => {
+					return "M" + (d.target.y - nodeWidth / 2) + "," + d.target.x +
+						"C" + (d.source.y + nodeWidth / 2 + 100) + "," + d.target.x +
+						" " + (d.source.y + nodeWidth / 2 + 100) + "," + d.source.x +
+						" " + (d.source.y) + "," + d.source.x;
+				});
 
 			// Transition exiting nodes to the parent's new position.
 			link.exit().transition(transition).remove()
 				.attr("d", d => {
-					const o = { y: source.x, x: source.y };
+					const o = { y: source.x0, x: source.y0 };
 					return diagonal({ source: o, target: o });
 				});
 
